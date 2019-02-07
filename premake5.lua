613d4d43971937fa9b403f71e00da528e1facdec
Damien Henderson <damien.henderson97@gmail.com>
2019-02-07 14:47:26 +0000

    Project Setup with Premake5

---
+++ premake5.lua
@@ -0,0 +1,63 @@
+-- Used for Visual Studio to determine the windows sdk version
+system_ver = "10.0.17763.0"
+
+-- Used to make the output directory nicer than the VS default
+output_format = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+workspace "SDIM"
+configurations {"Debug", "Release"}
+platforms {"x86", "x86_64"}
+
+
+
+project "SDIMCompiler"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    location "%{prj.name}/"
+    targetdir("bin/" .. output_format .. "/%{prj.name}")
+    objdir("int/" .. output_format .. "/%{prj.name}")
+
+    files {"%{prj.name}/**.cpp", "%{prj.name}/**.hpp"}
+
+    filter "configurations:Debug"
+        defines {"SDIMC_DEBUG"}
+        symbols "On"
+    filter "configurations:Release"
+        defines {"SDIMC_RELEASE"}
+        optimize "On"
+    filter "platforms:x86"
+        architecture "x86"
+        defines {"SDIMC_X86"}
+    filter "platforms:x86_64"
+        architecture "x86_64"
+        defines {"SDIMC_X86_64"}
+    filter "system:windows"
+        systemversion(system_ver)
+    filter {}
+
+project "SDIMVirtualMachine"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++17"
+    location "%{prj.name}/"
+    targetdir("bin/" .. output_format .. "/%{prj.name}")
+    objdir("int/" .. output_format .. "/%{prj.name}")
+
+    files {"%{prj.name}/**.cpp", "%{prj.name}/**.hpp"}
+
+    filter "configurations:Debug"
+        defines {"SDIMVM_DEBUG"}
+        symbols "On"
+    filter "configurations:Release"
+        defines {"SDIMVM_RELEASE"}
+        optimize "On"
+    filter "platforms:x86"
+        architecture "x86"
+        defines {"SDIMVM_X86"}
+    filter "platforms:x86_64"
+        architecture "x86_64"
+        defines {"SDIMVM_X86_64"}
+    filter "system:windows"
+        systemversion(system_ver)
+    filter {}
