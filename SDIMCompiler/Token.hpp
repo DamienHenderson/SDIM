1eb4fdd828eb4aa15fb4bab97b1b559d8a07dd31
Damien Henderson <damien.henderson97@gmail.com>
2019-02-15 20:48:55 +0000

    Added TokenTypes to enum

--- SDIMCompiler/Token.hpp
+++ SDIMCompiler/Token.hpp
@@ -2,8 +2,24 @@
 
 enum class TokenType
 {
-	
+	// Reserved Words
+	If, Else, ElseIf, While, Do, True, False,
+	Break, Return, Module, Include,
+
+	// Mathematical Operators
+	Add, Subtract, Divide, Multiply, Modulo, Brackets, LeftShift, RightShift, UnaryNegate,
+
+
+	// Logical Operators
+	Equivalent, NotEquivalent, GreaterThan, GreaterEqual, LessThan, LessEqual, LogicalNot,
+	BitwiseAnd, LogicalAnd, BitwiseOr, LogicalOr, BitwiseXor, LogicalXor, BitwiseNot,
+
+	// Miscellaneous
+	EOF, OpenScope, CloseScope
 };
+// TODO: Operator Precedence
+// Base operator precedence off of C++ to surprise users the least
+
 class Token
 {
 public:
