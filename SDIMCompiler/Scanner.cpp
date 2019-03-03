0eae4f5f428b10ff181bdec7809425123f1fe814
Damien Henderson <damien.henderson97@gmail.com>
2019-03-03 23:34:15 +0000

    Added scanning of two character tokens. TODO: keywords identifiers and literals

--- SDIMCompiler/Scanner.cpp
+++ SDIMCompiler/Scanner.cpp
@@ -20,11 +20,12 @@ namespace SDIM
 		std::string file_contents = ReadWholeFile(path);
 		std::cout << file_contents << "\n";
 		
-		auto MatchNext = [](const std::string& program_string, char match, size_t idx) -> bool
+		auto MatchNext = [&file_contents](char match, size_t idx) -> bool
 		{
-			return program_string[idx] == match;
+			return file_contents[idx] == match;
 		};
-		
+		// for keywords, identifiers and literals which may encompass several characters
+		Token current_token(TokenType::Unknown,"");
 		for (auto i = 0; i < file_contents.length(); i++)
 		{
 			char current_char = file_contents[i];
@@ -37,7 +38,12 @@ namespace SDIM
 				tokens.push_back(Token(TokenType::EOFToken, ConvertToString(current_char)));
 				break;
 			}
-			
+			// String literals may contain whitespace
+			if (IsWhitespace(current_char) && current_token.token_type != TokenType::StringLiteral)
+			{
+				// skip whitespace
+				++i;
+			}
 			switch (current_char)
 			{
 			case '(':
@@ -65,12 +71,30 @@ namespace SDIM
 				LogString(std::string("Extracted lexeme: ") + current_char);
 				break;
 			case '<':
-				tokens.push_back(Token(TokenType::LessThan, ConvertToString(current_char)));
-				LogString(std::string("Extracted lexeme: ") + current_char);
+				if (MatchNext('=', i + 1))
+				{
+					tokens.push_back(Token(TokenType::LessEqual, "<="));
+					++i;
+					LogString("Extracted lexeme: <=");
+				}
+				else
+				{
+					Token(TokenType::LessThan, ConvertToString(current_char));
+					LogString(std::string("Extracted lexeme: ") + current_char);
+				}
 				break;
 			case '>':
-				tokens.push_back(Token(TokenType::GreaterThan, ConvertToString(current_char)));
-				LogString(std::string("Extracted lexeme: ") + current_char);
+				if (MatchNext('=', i + 1))
+				{
+					tokens.push_back(Token(TokenType::GreaterEqual, ">="));
+					++i;
+					LogString("Extracted lexeme: >=");
+				}
+				else
+				{
+					Token(TokenType::GreaterThan, ConvertToString(current_char));
+					LogString(std::string("Extracted lexeme: ") + current_char);
+				}
 				break;
 			case ';':
 				tokens.push_back(Token(TokenType::SemiColon, ConvertToString(current_char)));
@@ -85,52 +109,157 @@ namespace SDIM
 				LogString(std::string("Extracted lexeme: ") + current_char);
 				break;
 			case '-':
-				tokens.push_back(Token(TokenType::Minus, ConvertToString(current_char)));
-				LogString(std::string("Extracted lexeme: ") + current_char);
+				if (MatchNext('=', i + 1))
+				{
+					tokens.push_back(Token(TokenType::MinusEqual, "-="));
+					++i;
+					LogString("Extracted lexeme: -=");
+				}
+				else
+				{
+					Token(TokenType::Minus, ConvertToString(current_char));
+					LogString(std::string("Extracted lexeme: ") + current_char);
+				}
 				break;
 			case '+':
-				tokens.push_back(Token(TokenType::Plus, ConvertToString(current_char)));
-				LogString(std::string("Extracted lexeme: ") + current_char);
+				if (MatchNext('=', i + 1))
+				{
+					tokens.push_back(Token(TokenType::PlusEqual, "+="));
+					++i;
+					LogString("Extracted lexeme: +=");
+				}
+				else
+				{
+					Token(TokenType::Plus, ConvertToString(current_char));
+					LogString(std::string("Extracted lexeme: ") + current_char);
+				}
 				break;
 			case '/':
-				tokens.push_back(Token(TokenType::ForwardSlash, ConvertToString(current_char)));
-				LogString(std::string("Extracted lexeme: ") + current_char);
+				if (MatchNext('=', i + 1))
+				{
+					tokens.push_back(Token(TokenType::SlashEqual, "/="));
+					++i;
+					LogString("Extracted lexeme: /=");
+				}
+				else
+				{
+					Token(TokenType::ForwardSlash, ConvertToString(current_char));
+					LogString(std::string("Extracted lexeme: ") + current_char);
+				}
 				break;
 			case '*':
-				tokens.push_back(Token(TokenType::Asterisk, ConvertToString(current_char)));
-				LogString(std::string("Extracted lexeme: ") + current_char);
+				if (MatchNext('=', i + 1))
+				{
+					tokens.push_back(Token(TokenType::AsteriskEqual, "*="));
+					++i;
+					LogString("Extracted lexeme: *=");
+				}
+				else
+				{
+					Token(TokenType::Asterisk, ConvertToString(current_char));
+					LogString(std::string("Extracted lexeme: ") + current_char);
+				}
 				break;
 			case '%':
-				tokens.push_back(Token(TokenType::Percent, ConvertToString(current_char)));
-				LogString(std::string("Extracted lexeme: ") + current_char);
+				if (MatchNext('=', i + 1))
+				{
+					tokens.push_back(Token(TokenType::PercentEqual, "%="));
+					++i;
+					LogString("Extracted lexeme: %=");
+				}
+				else
+				{
+					Token(TokenType::Percent, ConvertToString(current_char));
+					LogString(std::string("Extracted lexeme: ") + current_char);
+				}
 				break;
 			case '&':
-				tokens.push_back(Token(TokenType::Ampersand, ConvertToString(current_char)));
-				LogString(std::string("Extracted lexeme: ") + current_char);
+				if (MatchNext('=', i + 1))
+				{
+					tokens.push_back(Token(TokenType::AmpersandEqual, "&="));
+					++i;
+					LogString("Extracted lexeme: &=");
+				}
+				else if (MatchNext('|', i + 1))
+				{
+					tokens.push_back(Token(TokenType::DoubleAmpersand, "&&"));
+					++i;
+					LogString("Extracted lexeme: &&");
+				}
+				else
+				{
+					Token(TokenType::VerticalBar, ConvertToString(current_char));
+					LogString(std::string("Extracted lexeme: ") + current_char);
+				}
 				break;
 			case '|':
-				tokens.push_back(Token(TokenType::VerticalBar, ConvertToString(current_char)));
-				LogString(std::string("Extracted lexeme: ") + current_char);
+				if (MatchNext('=', i + 1))
+				{
+					tokens.push_back(Token(TokenType::VerticalBarEqual, "|="));
+					++i;
+					LogString("Extracted lexeme: |=");
+				}
+				else if (MatchNext('|', i + 1))
+				{
+					tokens.push_back(Token(TokenType::DoubleVerticalBar, "||"));
+					++i;
+					LogString("Extracted lexeme: ||");
+				}
+				else
+				{
+					Token(TokenType::VerticalBar, ConvertToString(current_char));
+					LogString(std::string("Extracted lexeme: ") + current_char);
+				}
 				break;
 			case '^':
-				tokens.push_back(Token(TokenType::Caret, ConvertToString(current_char)));
-				LogString(std::string("Extracted lexeme: ") + current_char);
+				if (MatchNext('=', i + 1))
+				{
+					tokens.push_back(Token(TokenType::CaretEqual, "^="));
+					++i;
+					LogString("Extracted lexeme: ^=");
+				}
+				else
+				{
+					Token(TokenType::Caret, ConvertToString(current_char));
+					LogString(std::string("Extracted lexeme: ") + current_char);
+				}
 				break;
 			case '!':
-				tokens.push_back(Token(TokenType::Bang, ConvertToString(current_char)));
-				LogString(std::string("Extracted lexeme: ") + current_char);
+				if (MatchNext('=', i + 1))
+				{
+					tokens.push_back(Token(TokenType::BangEqual, "!="));
+					++i;
+					LogString("Extracted lexeme: !=");
+				}
+				else
+				{
+					Token(TokenType::Bang, ConvertToString(current_char));
+					LogString(std::string("Extracted lexeme: ") + current_char);
+				}
 				break;
 			case '~':
-				tokens.push_back(Token(TokenType::Tilde, ConvertToString(current_char)));
-				LogString(std::string("Extracted lexeme: ") + current_char);
+				if (MatchNext('=', i + 1))
+				{
+					tokens.push_back(Token(TokenType::TildeEqual, "~="));
+					++i;
+					LogString("Extracted lexeme: ~=");
+				}
+				else
+				{
+					Token(TokenType::Tilde, ConvertToString(current_char));
+					LogString(std::string("Extracted lexeme: ") + current_char);
+				}
 				break;
 				 
 			default:
+				// TODO: remove once keywords and identifiers are processed
 				LogString(std::string("Unknown char: ") + current_char);
 				break;
 			}
 		}
-
+		// TODO: keywords
+		// TODO: identifiers (types and variables)
+		// TODO: literals
 		return true;
 	}
 
